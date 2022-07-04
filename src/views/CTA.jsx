e49d83d1d864bf78e3bb8da0de724a1d83d19b49
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-07-04 11:56:51 +0530

    updated

--- src/views/CTA.jsx
+++ src/views/CTA.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Resume from "../assets/Resume.pdf";
-const CTA = () => {
-  return (
-    <div className="cta">
-      <a href={Resume} download className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
-       Download Resume
-      </a>
+// import React from "react";
+// import Resume from "../assets/Resume.pdf";
+// const CTA = () => {
+//   return (
+//     <div className="cta">
+//       <a href={Resume} download className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
+//        Download Resume
+//       </a>
       
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default CTA;
+// export default CTA;
