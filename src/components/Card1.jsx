50bcd0bb8fd83b84ac9157138f25432993138571
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-09-14 16:56:12 +0530

    update

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -48,8 +48,9 @@ const Card1 = () => {
           </svg>
         </a>
         <div class="mt-2">
+        {/* https://bebodywise-clone-black.vercel.app/ */}
         <a
-          href="https://bebodywise-clone-black.vercel.app/"  target="__blank"
+          href="https://bebodywisediversityclone.netlify.app/index.html"  target="__blank"
           class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300"
         >
          View
